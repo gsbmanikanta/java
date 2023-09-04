94ec618f99b40dd8577c6db52e487a7536fb0ffc
gsbmanikanta <143478124+gsbmanikanta@users.noreply.github.com>
2023-09-03 22:48:30 -0700

    Create MYclass.java

---
+++ MYclass.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class MYclass {
+    public static void main(String args[]) {
+        int i1=10;
+        int i2=27;
+        int sum=i1+i2;
+        System.out.println("The sum of numbers=",+sum);
+    }
+}
