f4d890bb57a4a49897d03249cc1548dc05682436
gsbmanikanta <143478124+gsbmanikanta@users.noreply.github.com>
2023-09-07 15:54:07 +0530

    Add files via upload

---
+++ DefaultconstructorDemo.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class Rectangle{
+    int l,b,a;
+    Rectangle(){
+     l=5;
+     b=8;
+     a=l*b;
+    }
+    }
+    public class DefaultconstuctorDemo {
+        public static void main(String args[]){
+            Rectangle r1=new Rectangle();
+            Rectangle r2=new Rectangle();
+             System.out.println("Area of Rectangle 1 is"+r1.a);
+              System.out.println("Area of Rectangle 2 is"+r2.a);
+        }
+        }
+    
+    
