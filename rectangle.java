bb0a76e9c1eff59de11e4ef222e7effc852cbfb7
gsbmanikanta <143478124+gsbmanikanta@users.noreply.github.com>
2023-10-03 21:40:06 -0700

    Add files via upload

---
+++ rectangle.java
@@ -0,0 +1,20 @@
+//5-9-23
+//Rectangle.java
+import java.io.*;
+import java.util.*;
+public class Rectangle{
+int l,b,a;
+Rectangle(){
+l=5;
+b=8;
+a=i*b;
+}
+}
+public class DefaultDemo{
+public static void main(String args[]){
+Rectangle r1=new Rectangle();
+Rectangle r2=new Recatangle();
+System.out.println("Area of rectangle 1 is"+r1.a);
+System.out.println("Area of rectangle 2 is"+r1.a);
+}
+}
