49aabecb5be7d3a0e5f9c1f25a696014b9fdd67b
gsbmanikanta <143478124+gsbmanikanta@users.noreply.github.com>
2023-10-30 21:27:31 -0700

    Mathclass.java

---
+++ Mathclass.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+public class javaMathDemo{
+    public static void main(String args[]){
+        double x=28;
+        double y=4;
+        System.out.println("Maximum of x and y is "+Math.max(x,y));
+        System.out.println("Manimum of x and y is "+Math.min(x,y));
+        System.out.println("Square root of x and y is "+Math.sqrt(y));
+        System.out.println("power of x and y is "+Math.pow(x,y));
+        System.out.println("Logrthim of x and y is "+Math.log(x));
+        System.out.println("log10(x) is "+Math.log10(x));
+    }
+}
